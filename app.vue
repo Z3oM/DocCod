8f1a87f1e2e12c67c0bb7c3e1d925e3b81f1707b
Z3oM <programmater@gmail.com>
2022-04-13 23:54:16 +1000

    After first commit

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    <NuxtWelcome />
+
   </div>
 </template>
+
+<script lang="ts">
+
+const myN:Number = 50;
+
+function myFunc(NnNN:Number):String {
+    return "LOOOL"
+}
+
+console.log(myFunc(50));
+
+
+</script>
+
+<style lang="scss">
+
+</style>
